41829fd4e9a90fb920d66a3340251df35647acfb
Pavel Koryakin <xtr@xakep.ru>
2014-04-29 16:58:58 +0400

    Move Api to the separate file

--- backupd.py
+++ backupd.py
@@ -2,76 +2,14 @@
 import json
 import pickle
 import platform
-from random import random
 from uuid import uuid1
-from httplib import HTTPSConnection
-from urllib import urlencode
 from hashlib import sha512 as sha
 
 from daemon.runner import DaemonRunner
 
 from config import Config
 from filesystem import FSNode
-
-
-class Api(object):
-    BOUNDARY = '-' * 20 + sha(str(random())).hexdigest()[:20]
-    
-    def __init__(self, host, port, uuid, key):
-        self.conn = HTTPSConnection(host, port)
-        self.base_params = {'uuid': uuid, 'key': key}
-    
-    def _send(self, path, data={}, files={}, method='POST'):
-        data.update(self.base_params)
-        headers = {'Accept': 'text/plain'}
-        if files:
-            body = self.encode_multipart_data(data, files)
-            headers['Content-type'] = 'multipart/form-data; boundary=%s' % Api.BOUNDARY
-        else:
-            body = urlencode(data)
-            headers['Content-type'] = 'application/x-www-form-urlencoded'
-        self.conn.request(method,
-                          '/api/%s/' % path,
-                          body,
-                          headers)
-        response = self.conn.getresponse()
-        result = (response.status, response.read())
-        self.conn.close()
-        return result
-    
-    def encode_multipart_data(self, data={}, files={}):
-        """ Returns multipart/form-data encoded data
-        """
-        data.update(self.base_params)
-        boundary = '--' + Api.BOUNDARY
-        crlf = '\r\n'
-        
-        data_tpl = crlf.join((boundary,
-                                'Content-Disposition: form-data; name="%(name)s"',
-                                '',
-                                '%(value)s'))
-
-        file_tpl = crlf.join((boundary,
-                                'Content-Disposition: form-data; name="%(name)s"; filename="%(name)s"',
-                                'Content-Type: application/octet-stream',
-                                '',
-                                '%(value)s'))
-        
-        def render(tpl, data):
-            return [tpl % {'name': key,
-                           'value': value} for key, value in data.iteritems()]
-        
-        result = render(data_tpl, data)
-        if files:
-            result.extend(render(file_tpl, files))
-        result.append('%s--\r\n' % boundary)
-        return crlf.join(result)
-    
-    def hi(self, uname):
-        return self._send('hi', {'host': uname[1], 'uname': ' '.join(uname)})
-    
-    def set_fs(self, fs):
-        return self._send('set_fs', files={'fs': fs})
+from api import Api
 
 
 class App(object):
