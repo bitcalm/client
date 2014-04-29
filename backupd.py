139aca1ca4f67765faf2c9af7478a9bb3991076a
Pavel Koryakin <xtr@xakep.ru>
2014-04-29 16:46:00 +0400

    Add FS sending

--- backupd.py
+++ backupd.py
@@ -1,40 +1,80 @@
 #!/usr/bin/python
+import json
 import pickle
 import platform
+from random import random
 from uuid import uuid1
 from httplib import HTTPSConnection
 from urllib import urlencode
+from hashlib import sha512 as sha
 
 from daemon.runner import DaemonRunner
 
-from config.base import Config
+from config import Config
+from filesystem import FSNode
 
 
-class Api():
-    BASE_URL = '/api/'
-    HEADERS = {'Content-type': 'application/x-www-form-urlencoded',
-               'Accept': 'text/plain'}
+class Api(object):
+    BOUNDARY = '-' * 20 + sha(str(random())).hexdigest()[:20]
     
     def __init__(self, host, port, uuid, key):
         self.conn = HTTPSConnection(host, port)
         self.base_params = {'uuid': uuid, 'key': key}
     
-    def encode_data(self, data):
+    def _send(self, path, data={}, files={}, method='POST'):
         data.update(self.base_params)
-        return urlencode(data)
+        headers = {'Accept': 'text/plain'}
+        if files:
+            body = self.encode_multipart_data(data, files)
+            headers['Content-type'] = 'multipart/form-data; boundary=%s' % Api.BOUNDARY
+        else:
+            body = urlencode(data)
+            headers['Content-type'] = 'application/x-www-form-urlencoded'
+        self.conn.request(method,
+                          '/api/%s/' % path,
+                          body,
+                          headers)
+        response = self.conn.getresponse()
+        result = (response.status, response.read())
+        self.conn.close()
+        return result
+    
+    def encode_multipart_data(self, data={}, files={}):
+        """ Returns multipart/form-data encoded data
+        """
+        data.update(self.base_params)
+        boundary = '--' + Api.BOUNDARY
+        crlf = '\r\n'
+        
+        data_tpl = crlf.join((boundary,
+                                'Content-Disposition: form-data; name="%(name)s"',
+                                '',
+                                '%(value)s'))
+
+        file_tpl = crlf.join((boundary,
+                                'Content-Disposition: form-data; name="%(name)s"; filename="%(name)s"',
+                                'Content-Type: application/octet-stream',
+                                '',
+                                '%(value)s'))
+        
+        def render(tpl, data):
+            return [tpl % {'name': key,
+                           'value': value} for key, value in data.iteritems()]
+        
+        result = render(data_tpl, data)
+        if files:
+            result.extend(render(file_tpl, files))
+        result.append('%s--\r\n' % boundary)
+        return crlf.join(result)
     
     def hi(self, uname):
-        self.conn.request('POST',
-                          Api.BASE_URL + 'hi/',
-                          self.encode_data({'host': uname[1],
-                                            'uname': ' '.join(uname)}),
-                          Api.HEADERS)
-        res = self.conn.getresponse()
-        self.conn.close()
-        return res
+        return self._send('hi', {'host': uname[1], 'uname': ' '.join(uname)})
+    
+    def set_fs(self, fs):
+        return self._send('set_fs', files={'fs': fs})
 
 
-class App():
+class App(object):
     SETTINGS_PATH = '/var/lib/bitcalm/data'
     
     def __init__(self):
@@ -57,21 +97,34 @@ class App():
                     pickle.dump(data, f)
         self.key = data['key']
         self.is_registered = data['registered']
+        self.fshash = data.get('fshash', None)
     
     def save_settings(self):
         with open(App.SETTINGS_PATH, 'w') as f:
-            pickle.dump({'key': self.key, 'registered': self.is_registered}, f)
+            pickle.dump({'key': self.key,
+                         'registered': self.is_registered,
+                         'fshash': self.fshash}, f)
     
     def run(self):
-        pass
+        root = FSNode('/', ignore=('sys', 'dev', 'root', 'cdrom',
+                                   'boot', 'lost+found', 'proc', 'tmp',
+                                   'sbin', 'bin'))
+        root_d = root.as_dict()
+        root_str = json.dumps(root_d)
+        h = sha(root_str).hexdigest()
+        if not self.fshash or self.fshash != h:
+            status, content = self.api.set_fs(root_str)
+            if status == 200:
+                self.fshash = h
+                self.save_settings()
 
 app = App()
 
 if not app.is_registered:
     print 'Sending info about new client...'
-    res = app.api.hi(platform.uname())
-    print res.read()
-    if res.status == 200:
+    status, content = app.api.hi(platform.uname())
+    print content
+    if status == 200:
         app.is_registered = True
         app.save_settings()
     else:
