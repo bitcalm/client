87ea2bf391f60c84b639d81b660f8acf51739889
Pavel Koryakin <xtr@xakep.ru>
2014-08-22 12:15:20 +0400

    Remove unused backup code

--- bitcalm/backup.py
+++ bitcalm/backup.py
@@ -1,8 +1,6 @@
 import os
 import math
 import tarfile
-import calendar
-from datetime import datetime, date, timedelta
 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
@@ -14,77 +12,16 @@ from bitcalm.config import status
 TMP_FILEPATH = '/tmp/backup.tar.gz'
 CHUNK_SIZE = 32 * 1024 * 1024
 
-class SCHEDULE:
-    DAILY = 'daily'
-    WEEKLY = 'weekly'
-    MONTHLY = 'monthly'
-    TYPES = (DAILY, WEEKLY, MONTHLY)
-
-
-def next_daily():
-    period = status.schedule.get(SCHEDULE.DAILY)
-    if not period:
-        return None
-    if not status.prev_backup:
-        return datetime.utcnow()
-    prev_date = date(*map(int, status.prev_backup.split('.')))
-    next_date = prev_date + timedelta(days=period)
-    return datetime(next_date.year,
-                    next_date.month,
-                    next_date.day,
-                    *status.schedule['time'])
-
-
-def next_weekly():
-    days = status.schedule.get(SCHEDULE.WEEKLY)
-    if not days:
-        return None
-    today = date.today()
-    today_index = today.isoweekday() % 7
-    
-    backup_days = []
-    for i in range(7):
-        if days & 1 << i:
-            backup_days.append(i)
-    
-    curr_week = filter(lambda x, t=today_index: x >= t, backup_days)
-    if status.prev_backup == today.strftime('%Y.%m.%d'):
-        curr_week = curr_week[1:]
-    next_day = curr_week[0] if curr_week else backup_days[0] + 7
-    next_date = today + timedelta(days=next_day-today_index)
-    return datetime(next_date.year,
-                    next_date.month,
-                    next_date.day,
-                    *status.schedule['time'])
-
-
-def next_monthly():
-    day = status.schedule.get(SCHEDULE.MONTHLY)
-    if not day:
-        return None
-    today = date.today()
-    month = today.month if day >= today.day else today.month + 1
-    year = today.year if month >= today.month else today.year + 1
-    if day >= 29:
-        day = min(calendar.monthrange(year, month)[1], day)
-    return datetime(year, month, day, *status.schedule['time'])
-
-
-def get_next():
-    if not status.schedule:
-        return None
-    for key in SCHEDULE.TYPES:
-        if key in status.schedule:
-            func = {SCHEDULE.DAILY: next_daily,
-                    SCHEDULE.WEEKLY: next_weekly,
-                    SCHEDULE.MONTHLY: next_monthly}
-            return func[key]
-    return None
-
 
 def next_date():
-    func = get_next()
-    return func() if func else None
+    s = next_schedule()
+    return s.next_backup if s else None
+
+
+def next_schedule():
+    if status.schedules:
+        return min(status.schedules)
+    return None
 
 
 def get_bucket():
